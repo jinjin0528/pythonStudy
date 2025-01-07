791d5b83f9402dc1b89750d18af291817404ab79
이효진 <jin12lee@naver.com>
2025-01-07 17:17:26 +0900

    if문 공부하기

---
+++ practice_if.py
@@ -0,0 +1,22 @@
+# today = 'Mon'
+#
+# if today == 'Mon':
+# 	print('월요일입니다')
+# else:
+#     print('월요일이 아니에요')
+# print('그렇군요')
+
+num = 20  # 횟수
+
+if num >= 10:
+        print('10번 이상')
+        if num >= 20:
+            print('20번 이상')
+        elif num >= 30:
+                print('30번 이상')
+        else:
+                print('더 하세요')
+else:
+        print('분발하세요')
+
+print('네')
