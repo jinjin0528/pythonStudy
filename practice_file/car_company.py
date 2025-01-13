ac0ffb5e1c7dc9d230041d8652b015bfb0996619
이효진 <jin12lee@naver.com>
2025-01-13 23:06:27 +0900

    인프런 oop 강의 연습

---
+++ practice_file/car_company.py
@@ -0,0 +1,40 @@
+# 필기 : 클래스 중심 -> 데이터 중심 -> 객체로 관리
+
+# 이렇게 리스트로 따로 관리할 경우 회사가 사라지거나 상품이 추가되면 일일이 관리해야 한다는 문제가 있다.
+# 차량 1
+car_company_1 = {'Ferrari'}
+car_detail_1 = [
+    {'color': 'White'},
+    {'housepower': 500},
+    {'price': 8000}
+]
+# 차량 2
+car_company_2 = {'BMW'}
+car_detail_2 = [
+    {'color': 'Black'},
+    {'housepower': 300},
+    {'price': 5000}
+]
+# 차량 3
+car_company_3 = {'Audi'}
+car_detail_3 = [
+    {'color': 'Silver'},
+    {'housepower': 400},
+    {'price': 6000}
+]
+
+# 리스트 구조
+car_company_list = ['Ferrari', 'BMW', 'Audi']
+car_detail_list = [
+    {'color': 'White', 'housepower': 500, 'price': 8000},
+    {'color': 'Black', 'housepower': 300, 'price': 5000},
+    {'color': 'Silver', 'housepower': 400, 'price': 6000},
+]
+
+del car_company_list[1]
+del car_detail_list[1]
+
+print(car_company_list)
+print(car_detail_list)
+print()
+print()
