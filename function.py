9e3304ce8cbff5551b8b908d94e8e93e5b44257e
이효진 <jin12lee@naver.com>
2025-01-16 16:16:44 +0900

    입력

--- function.py
+++ function.py
@@ -1,3 +1,11 @@
 customer = '나손님'
 show_price = 10000
 print(f'{customer} 손님 커트 가격 {show_price}원 입니다.')
+
+name = input('성함을 입력하시오 : ')
+print(name)
+people = int(input('인원을 입력하시오 : '))
+if people > 5 :
+   print('최대 4명만 가능합니다.')
+else:
+   print('예약이 완료되었습니다.')
