684bbee3ef6f8da517771a190f2a8773a5e7c052
이효진 <jin12lee@naver.com>
2025-02-03 14:07:20 +0900

    [BRONZE level 5] 오늘 날짜

---
+++ BRONZE/backjoon_10699.py
@@ -0,0 +1,8 @@
+import datetime
+now = datetime.datetime.now()
+print(f"{now.year}-{now.month}-{now.day}")
+
+# datetime. 줄이기 위해 from datetime 사용
+# from datetime import datetime
+# now = datetime.now().date()
+print(now)
