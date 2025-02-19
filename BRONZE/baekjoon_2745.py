d25d9e36bb08b058c371a34c2eb55858ae21bfbe
이효진 <jin12lee@naver.com>
2025-02-19 11:45:38 +0900

    [BRONZE level 2] 진법 변환.  memory 32544 KB.  time 40 ms

---
+++ BRONZE/baekjoon_2745.py
@@ -0,0 +1,10 @@
+# num_list = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+# n,b = input().split()
+# answer = 0
+#
+# for i, num in enumerate(n[::-1]):
+#     answer += int(b) ** i *num_list.index(num)
+# print(answer)
+
+n, b = input().split()
+print(int(n, int(b)))
