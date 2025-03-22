5ce71404a046e76afdf5bacbf9f43050395e0af1
이효진 <jin12lee@naver.com>
2025-03-23 00:01:23 +0900

    [BRONZE level 3] Angles.  memory 32412 KB.  time 36 ms

--- BRONZE/baekjoon_15178.py
+++ BRONZE/baekjoon_15178.py
@@ -3,6 +3,6 @@ n = int(input())
 for _ in range(n):
     a,b,c = map(int, input().split())
     if a+b+c != 180:
-        print(a,b,c,'Check')
+        print(a, b, c, 'Check')
     else:
         print(a, b, c, "Seems OK")
