03d7494c36de4c88dfbb807cac3b33823aa56bbb
이효진 <jin12lee@naver.com>
2025-04-23 12:52:22 +0900

    [BRONZE level 5] 이 별은 무슨 색일까.  memory 32412 KB.  time 36 ms

---
+++ BRONZE/baekjoon_30676.py
@@ -0,0 +1,16 @@
+star = int(input())
+
+if 380 <= star < 425:
+    print("Violet")
+elif 425 <= star < 450 :
+    print("Indigo")
+elif 450 <= star < 495:
+    print("Blue")
+elif 495 <= star < 570 :
+    print("Green")
+elif 570 <= star < 590 :
+    print("Yellow")
+elif 590 <= star < 620 :
+    print("Orange")
+elif 620 <= star <= 780 :
+    print("Red")
