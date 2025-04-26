77af52321500f57b519b002c1462707f258a10e4
이효진 <jin12lee@naver.com>
2025-04-26 23:25:55 +0900

    [BRONZE level 4] 수학은 체육과목 입니다 2.  memory 32412 KB.  time 40 ms

---
+++ BRONZE/baekjoon_17362.py
@@ -0,0 +1,18 @@
+n = int(input())
+
+if n % 8 == 1:
+    print(1)  # 엄지
+elif n % 8 == 2:
+    print(2)  # 검지
+elif n % 8 == 3:
+    print(3)  # 중지
+elif n % 8 == 4:
+    print(4)  # 약지
+elif n % 8 == 5:
+    print(5)  # 새끼손가락
+elif n % 8 == 6:
+    print(4)  # 약지
+elif n % 8 == 7:
+    print(3)  # 중지
+elif n % 8 == 0:
+    print(2)  # 검지
